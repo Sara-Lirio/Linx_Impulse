bd1aac843afae1eae1485ad56181824ec53c8a91
Sara <saraeliza.lirio@gmail.com>
2022-08-17 10:41:59 -0300

    Component Button importado para o component Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 const Header = () => {
   return (
     <div>
+        <p>uma seleção de produtos</p>
         <h1>especial para você</h1>
+        <p>Todos os produtos desta lista foram selecionados a partir de sua navegação. Aproveite!</p>
+
+        <div>
+            <Button text='Conheça a Linx'></Button>
+            <Button text='Ajude o algorítimo'></Button>
+            <Button text='Seus produtos'></Button>
+            <Button text='Compartilhe'></Button>
+        </div>
     </div>
   )
 }
