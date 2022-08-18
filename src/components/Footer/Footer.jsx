5de5039b91327cd01d710047b8e94191fbf7e558
Sara <saraeliza.lirio@gmail.com>
2022-08-18 11:54:05 -0300

    Finalizando estilização do componente do rodapé

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,14 @@ import style from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <div className={style.footer}>
-            <Text text='Testando suas habilidades em HTML, CSS e JS.' />
-            <Text text='Linx Impulse' />
-            <Text text='2019' />
-        </div>
+        <footer className={style.footer}>
+            <div className={style.triangulediv}></div>
+            <div className={style.text}>
+                <Text text='Testando suas habilidades em HTML, CSS e JS.' />
+                <Text text='Linx Impulse' />
+                <Text text='2019' />
+            </div>
+        </footer>
     )
 }
 
